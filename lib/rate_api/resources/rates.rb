9948136750c9ef2e038820febb014e3b3bf62a0b
Michał Bronikowski <michal.bronikowski@netguru.com>
2023-04-14 13:24:23 +0200

    Refactor rates fetcher

--- lib/rate_api/resources/rates.rb
+++ lib/rate_api/resources/rates.rb
@@ -7,15 +7,21 @@ module RateApi
   module Resources
     class Rates < Base
       def get(currency)
-        response = RestClient::Request.execute(
-          method: :get,
-          url: endpoint_url("/v6/latest/#{CGI.escape(currency)}")
-        )
-        parsed_response = parse(response)
+        parsed_response = fetch_rates(currency)
         return Response.new(data: parsed_response[:rates]) if success?(parsed_response)
         Response.new(error: extract_error(parsed_response))
       rescue RestClient::BadRequest, RestClient::NotFound
         Response.new(error: "external API error")
+      end
+
+      private
+
+      def fetch_rates(currency)
+        response = RestClient::Request.execute(
+          method: :get,
+          url: endpoint_url("/v6/latest/#{CGI.escape(currency)}")
+        )
+        parse(response)
       end
     end
   end
